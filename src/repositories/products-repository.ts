3df8a9291a26aa894f776d61bf6fdf24c650306b
Mark_Maistrenko <revanmayfall@gmail.com>
2023-01-26 20:00:12 +0400

    +async

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -1,10 +1,14 @@
-const products = [
+export type ProductType = {
+  id: number;
+  title: string;
+};
+const products: ProductType[] = [
   { id: 1, title: "tomato" },
   { id: 2, title: "orange" },
 ];
 
 export const productsRepository = {
-  findProducts(title: string | null | undefined) {
+  async findProducts(title: string | null | undefined): Promise<ProductType[]> {
     if (title) {
       let filteredProducts = products.filter(
         (p) => p.title.indexOf(title) > -1
@@ -18,12 +22,12 @@ export const productsRepository = {
     let product = products.find((p) => p.id === id);
     return product;
   },
-  createProduct(title: string) {
+  async createProduct(title: string):Promise<ProductType> {
     const newProduct = { id: +new Date(), title: title };
     products.push(newProduct);
-    return newProduct;
+    return newProduct; 
   },
-  updateProduct(id: number, title: string) {
+ async updateProduct(id: number, title: string):Promise<boolean> {
     let product = products.find((p) => p.id === id);
     if (product) {
       product.title = title;
--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -1,18 +1,24 @@
 import express, { request, Request, Response } from "express";
 
 import { Router } from "express";
-import { productsRepository } from "../repositories/products-repository";
-import {body, validationResult} from "express-validator";
+import {
+  productsRepository,
+  ProductType,
+} from "../repositories/products-repository";
+import { body, validationResult } from "express-validator";
 import { inputValidationMiddleware } from "../midlewares/input-validation-middleware";
 
 export const productsRouter = Router({});
 
-const titleValidation = body('title').trim().isLength({min: 3, max:10}).withMessage('Title error ');
+const titleValidation = body("title")
+  .trim()
+  .isLength({ min: 3, max: 10 })
+  .withMessage("Title error ");
 
-productsRouter.get("/", (req: Request, res: Response) => {
-  const foundProducts = productsRepository.findProducts(
-    req.query.title?.toString()
-  );
+productsRouter.get("/", async (req: Request, res: Response) => {
+  const foundProductsPromise: Promise<ProductType[]> =
+    productsRepository.findProducts(req.query.title?.toString());
+  const foundProducts: ProductType[] = await foundProductsPromise;
   res.send(foundProducts);
 });
 
@@ -35,25 +41,30 @@ productsRouter.delete("/:id", (req: Request, res: Response) => {
   }
 });
 
-productsRouter.post("/",
-titleValidation,inputValidationMiddleware,
-(req: Request, res: Response) => {
-
-  
-  const newProduct = productsRepository.createProduct(req.body.title);
-  res.status(201).send(newProduct);
-});
-
-productsRouter.put("/:id",titleValidation, inputValidationMiddleware, (req: Request, res: Response) => {
-
-  const isUpdated = productsRepository.updateProduct(
-    +req.params.id,
-    req.body.title
-  );
-  if (isUpdated) {
-    const product = productsRepository.findProductById(+req.params.id);
-    res.send(product);
-  } else {
-    res.send(404);
+productsRouter.post(
+  "/",
+  titleValidation,
+  inputValidationMiddleware,
+  async (req: Request, res: Response) => {
+    const newProduct:ProductType =await productsRepository.createProduct(req.body.title);
+    res.status(201).send(newProduct);
   }
-});
+);
+
+productsRouter.put(
+  "/:id",
+  titleValidation,
+  inputValidationMiddleware,
+  async (req: Request, res: Response) => {
+    const isUpdated = await productsRepository.updateProduct(
+      +req.params.id,
+      req.body.title
+    );
+    if (isUpdated) {
+      const product = productsRepository.findProductById(+req.params.id);
+      res.send(product);
+    } else {
+      res.send(404);
+    }
+  }
+);
